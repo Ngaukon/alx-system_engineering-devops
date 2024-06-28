2528f21cb781e002f740b06dd9b5b7cc2e2ae153
Ngaukon <josephlucia100@gmail.com>
2024-06-28 12:18:27 -0700

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# install the puppet-lint -v 2.5.0
+# Install puppet-lint version 2.5.0
+package { 'puppet-lint':
+  ensure => '2.5.0',
+}
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
