06ac7388c3dcca5a8f4ed78ebb681725853f76db
Ngaukon <josephlucia100@gmail.com>
2024-08-13 13:07:11 -0700

    to pten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Function thats goes on to  print hot posts on a given Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
+    """Print the title of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "Mozilla/5.0"
@@ -11,24 +12,10 @@ def top_ten(subreddit):
     params = {
         "limit": 10
     }
-    
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        
-        # Check if the response is successful
-        if response.status_code != 200:
-            print("None")
-            return
-        
-        # Try to parse the JSON response
-        results = response.json().get("data")
-        if results is None:
-            print("None")
-            return
-        
-        # Print the titles of the hot posts
-        [print(c.get("data").get("title")) for c in results.get("children")]
-        
-    except Exception as e:
-        # Catch any other unexpected errors
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
